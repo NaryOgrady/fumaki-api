301502e32ae1768171a75ff3404807bc37b5f20c
naryogrady <jluis.garza@live.com.mx>
2017-08-22 00:12:33 -0500

    Setup initial ORM configuration using SQLAchemy

--- src/main.py
+++ src/main.py
@@ -1,7 +1,27 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+import os
 
 app = Flask(__name__)
 
+engine = create_engine('sqlite:///data/fumaki.db')
+Base = declarative_base()
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class Actor(Base):
+    __tablename__ = 'actors'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    url = Column(String)
+    image = Column(String)
+
+
 @app.route("/")
 def hello_world():
-    return "test 3"  
+    return session.query(Actor).get(1).image
