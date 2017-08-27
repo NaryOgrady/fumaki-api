fa3859efa9f0cdb0e97a87ced398e0f2506152fc
naryogrady <jluis.garza@live.com.mx>
2017-08-27 18:08:06 -0500

    Add orm using sqlalchemy

--- src/main.py
+++ src/main.py
@@ -1,27 +1,11 @@
 from flask import Flask
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import sessionmaker
 
-import os
+from models.actor import Actor
+from service import actor_service as service
 
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///data/fumaki.db')
-Base = declarative_base()
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-class Actor(Base):
-    __tablename__ = 'actors'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    url = Column(String)
-    image = Column(String)
-
-
 @app.route("/")
 def hello_world():
-    return session.query(Actor).get(1).image
+    data = service.get_random_actors(3)
+    return ', '.join(actor.name for actor in data)
