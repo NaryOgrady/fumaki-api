c6fe95cf85a6edc140f167ab8c553da4f2ef8ca6
naryogrady <jluis.garza@live.com.mx>
2017-09-20 23:13:24 -0500

    Add cors package

--- src/main.py
+++ src/main.py
@@ -4,11 +4,13 @@ from routes import *
 from service import actor_service as service
 from helpers import json_helper as json
 from schemas.actor_schema import ActorSchema
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 
 @app.route(RANDOM_ACTORS)
-def hello_world(quantity):
+@cross_origin()
+def get_random_actors(quantity):
     actors_data = service.get_random_actors(quantity)
     schema = ActorSchema()
     json_data = json.serialize_query_list(actors_data, schema)
